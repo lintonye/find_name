3de13d1531952a2f815a796338963ec53def5e77
Linton Ye <linton@ikamobile.com>
2012-05-08 16:05:37 -0700

    test content

--- findname_web.rb
+++ findname_web.rb
@@ -1,14 +1,24 @@
 #!/usr/bin/ruby
-
-require_relative 'findname'
+#
+require './findname.rb'
 require 'pathname'
 
 def save(params, name)
   io = params[name].first
   out_dir = Pathname.new '.'
-  File.open out_dir+io.original_filename, 'w' {|f| f<<io.read}
+  File.open out_dir+io.original_filename, 'w' do |f| 
+    f<<io.read
+  end
 end
 
+puts "Content-Type: text/html"
+puts
+puts "<html>"
+puts "<body>"
+puts "<h1>Hello Ruby!</h1>"
+puts "</body>"
+puts "</html>"
+=begin
 cgi = CGI.new
 params = cgi.params
 
@@ -19,3 +29,4 @@ run_findname master_file, input_file
 cgi.header('Content-Disposition' => 'attachment;filename=output.csv')
 cgi.out 'application/zip' { File.open 'output.csv', 'r'}
 
+=end
