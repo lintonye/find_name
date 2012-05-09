5f05bbf136706f943bba5c2f57f03d1462c84b13
Linton Ye <linton@ikamobile.com>
2012-05-08 17:10:44 -0700

    Working!

--- findname_web.rb
+++ findname_web.rb
@@ -2,31 +2,34 @@
 #
 require './findname.rb'
 require 'pathname'
+require 'cgi'
 
+include Findname
+API = true
+OUT_DIR = Pathname.new '/tmp'
 def save(params, name)
   io = params[name].first
-  out_dir = Pathname.new '.'
-  File.open out_dir+io.original_filename, 'w' do |f| 
+  path = OUT_DIR+io.original_filename
+  File.open path, 'w' do |f| 
     f<<io.read
   end
+  path
 end
 
-puts "Content-Type: text/html"
-puts
-puts "<html>"
-puts "<body>"
-puts "<h1>Hello Ruby!</h1>"
-puts "</body>"
-puts "</html>"
-=begin
 cgi = CGI.new
 params = cgi.params
-
 master_file = save params, 'master'
 input_file = save params, 'input'
-run_findname master_file, input_file
 
-cgi.header('Content-Disposition' => 'attachment;filename=output.csv')
-cgi.out 'application/zip' { File.open 'output.csv', 'r'}
+#master_file = '/tmp/master.csv'
+#input_file = '/tmp/input.csv'
 
-=end
+output_path = OUT_DIR + 'output.csv'
+Findname.run master_file, input_file, output_path.to_s
+output = File.open(output_path, 'r').read
+
+cgi.out "Content-Type" => 'application/zip',
+   "content-disposition" => "attachment; filename=output.csv"  do 
+  output
+end
+
